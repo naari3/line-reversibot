4e8048d037b7297aa43cbc28bc5326ffdfd7dc60
naarisan <qazaqaza98@gmail.com>
2016-11-23 20:52:28 +0900

    v1

---
+++ reversi-bot.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from __future__ import unicode_literals
+
+import errno
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
+
+from io import BytesIO
+
+from flask import Flask, request, abort, send_file
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    ImagemapSendMessage, BaseSize, URIImagemapAction, MessageImagemapAction, ImagemapArea,
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+)
+
+from reversi import Reversi
+import random, re
+
+input_format = re.compile(r'[a-h][1-8]')
+
+reversies = {}
+
+app = Flask(__name__)
+
+hostname = "https://a80d2c42.ngrok.io"
+
+your_turn_format = "{}さんは{}です。"
+first_attack = "先攻"
+second_attack = "後攻"
+finish_format = "{} - {}\nあなたの{}"
+win_string = "勝ち"
+lose_string = "負け"
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+
+# function for create tmp dir for download content
+def make_static_tmp_dir():
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
+@app.route('/boards/<user_id>/<timestump>/<int:size>')
+def board_images(user_id=None, timestump=None, size=1040):
+    user = reversies.get(user_id, None)
+    if user is None or timestump is None:
+        return 'NG'
+    buf = BytesIO()
+    user.board_images[timestump][size].save(buf, 'png')
+    buf.seek(0,0)
+    return send_file(buf, mimetype='image/png')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+
+    if text == 'オセロ':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+        turn = random.randint(1,2)
+        reversies[profile.user_id] = Reversi(turn)
+        if turn == 2:
+            reversies[profile.user_id].ai_turn_proccess()
+        putable = reversies[profile.user_id].able_to_put()
+        timestump = reversies[profile.user_id].update_board_images()
+        textmessage = TextSendMessage(
+            text=your_turn_format.format(profile.display_name, (first_attack) if (turn == 1) else (second_attack))
+        )
+        print("/boards/{}/{}/{}".format(profile.user_id, timestump, 1040))
+        imagemap = make_reversi_imagemap(profile.user_id, timestump, putable)
+        try:
+            line_bot_api.reply_message(event.reply_token, [textmessage, imagemap])
+        except Exception as e:
+            print(e.error.details)
+
+    if input_format.match(text):
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+        x, y = text
+        x = ord(x) - 97 # str:[a-h] -> int:[0-7]
+        y = int(y) - 1 # int:[0-7]
+        p = y * 8 + x
+        reversies[profile.user_id].put_piece(p, reversies[profile.user_id].turn)
+        reversies[profile.user_id].ai_turn_proccess()
+        putable = reversies[profile.user_id].able_to_put()
+        if putable:
+            timestump = reversies[profile.user_id].update_board_images()
+            imagemap = make_reversi_imagemap(profile.user_id, timestump, putable)
+            line_bot_api.reply_message(event.reply_token, [imagemap])
+        else:
+            score1 = (reversies[profile.user_id]==1).sum()
+            score2 = (reversies[profile.user_id]==2).sum()
+            turn = reversies[profile.user_id].turn
+            if (score1 > score2 and turn == 1) or (score2 > score1 and turn == 2):
+                judge = True
+            else:
+                judge = False
+            textmessage = TextSendMessage(
+                text = finish_format.format(score1, score2, (win_string) if judge else (lose_string))
+            )
+            timestump = reversies[profile.user_id].update_board_images()
+            imagemap = make_reversi_imagemap(profile.user_id, timestump, putable)
+            line_bot_api.reply_message(event.reply_token, [imagemap, textmessage])
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
+
+
+# Other Message Type
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    else:
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save content.'),
+            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
+        ])
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='Got follow event'))
+
+@handler.add(UnfollowEvent)
+def handle_unfollow():
+    app.logger.info("Got Unfollow event")
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == 'ping':
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='pong'))
+
+
+def make_reversi_area(x, y):
+    area = ImagemapArea(
+        x = x * 130,
+        y = y * 130,
+        width = 130,
+        height = 130,
+    )
+    return area
+
+def make_reversi_action(p):
+    y, x = divmod(p, 8)
+    i2a = str.maketrans("01234567", "abcdefgh") # int to alphabet
+    text = '{}{}'.format(str(x).translate(i2a), y+1)
+    action = MessageImagemapAction(
+        text = text,
+        area = make_reversi_area(x, y),
+    )
+    return action
+
+def make_reversi_imagemap(user_id, timestump, putable):
+    imagemap_message = ImagemapSendMessage(
+        base_url=hostname + "/boards/{}/{}".format(user_id, timestump),
+        alt_text='reversi board',
+        base_size=BaseSize(height=1040, width=1040),
+        actions=[make_reversi_action(p) for p in putable]
+    )
+    return imagemap_message
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=options.debug, port=options.port)
---
+++ reversi.py
@@ -0,0 +1,178 @@
+import numpy as np
+from PIL import Image, ImageDraw
+import time
+import datetime
+
+BG = (0, 153, 76)
+White = (255, 255, 255)
+Black = (0, 0, 0)
+class Reversi(object):
+    """docstring for reversi."""
+    def __init__(self, turn=None):
+        self.sizes = [1040, 700, 460, 300, 240]
+        self.size_fixers = {
+            1040: {"image_size": (1040, 1040), "board_size": 1040, "square_size": 130, "piece_size_diff": 5, "side_space": 0},
+            700:  {"image_size": (700,  700),  "board_size": 696,  "square_size": 87,  "piece_size_diff": 2, "side_space": 2},
+            460:  {"image_size": (460,  460),  "board_size": 456,  "square_size": 57,  "piece_size_diff": 2, "side_space": 2},
+            300:  {"image_size": (300,  300),  "board_size": 296,  "square_size": 37,  "piece_size_diff": 2, "side_space": 2},
+            240:  {"image_size": (240,  240),  "board_size": 240,  "square_size": 30,  "piece_size_diff": 2, "side_space": 0},
+        }
+        if turn:
+            self.turn = turn
+            self.ai_turn = 3 - turn
+        self.board = self.create_board()
+        self.board_images = {}
+        self.create_board_images()
+        self.update_board_images()
+    def create_board(self):
+        a = np.zeros(64, dtype=int)
+        a[27] = a[36] = 1
+        a[28] = a[35] = 2
+        return a
+    def print_board(self, a):
+        print('  a b c d e f g h')
+        for i in range(8):
+            print(i+1, end=' ')
+            print(' '.join('.*o'[j] for j in a[i*8:][:8]))
+    def put_piece(self, p, w, puton=True, chk=True):
+        t, x, y = 0, p%8, p//8
+        for di, fi in zip([-1, 0, 1], [x, 7, 7-x]):
+            for dj, fj in zip([-8, 0, 8], [y, 7, 7-y]):
+                if not di == dj == 0:
+                    b = self.board[p+di+dj::di+dj][:min(fi, fj)]
+                    n = (b==3-w).cumprod().sum()
+                    if b.size <= n or b[n] != w: n = 0
+                    t += n
+                    if puton:
+                        b[:n] = w
+        if puton:
+            if chk: assert(self.board[p] == 0 and t > 0)
+            self.board[p] = w
+        return t
+    def best(self, w):
+        from math import exp
+        r, b, c = [], self.board.copy(), 1+exp(-np.count_nonzero(self.board)/16)
+        for i in range(64):
+            if b[i] != 0: continue
+            t = put_piece(b, i, w, True, False)
+            if t == 0:
+                b[i] = 0
+                continue
+            u = sum(b[j]==0 and put_piece(b, j, 3-w, False) > 0 for j in range(64))
+            r.append((t-c*u+np.random.rand()*0.5, i))
+            b = self.board.copy()
+        return sorted(r)[-1][1] if r else -1
+
+    def create_board_images(self):
+        for v in self.sizes:
+            image_size  = self.size_fixers[v]["image_size"]
+            board_size  = self.size_fixers[v]["board_size"]
+            square_size = self.size_fixers[v]["square_size"]
+            side_space  = self.size_fixers[v]["side_space"]
+            im = Image.new(mode="RGB",  size=image_size)
+            draw = ImageDraw.Draw(im)
+            draw.rectangle([(side_space, side_space), (board_size, board_size)], BG,)
+            for i in range(8):
+                draw.line([(i*square_size+side_space, side_space), (i*square_size+side_space, board_size+side_space)], fill=0)
+                draw.line([(side_space, i*square_size+side_space), (board_size+side_space, i*square_size+side_space)], fill=0)
+            self.board_images[v] = im
+
+    def put_piece_images(self, p, w, timestump):
+        for v in self.sizes:
+            board_image = self.board_images[v]
+            board_size  = self.size_fixers[v]["board_size"]
+            square_size = self.size_fixers[v]["square_size"]
+            side_space  = self.size_fixers[v]["side_space"]
+            piece_size_diff = self.size_fixers[v]["piece_size_diff"]
+            draw = ImageDraw.Draw(board_image)
+            y, x = divmod(p, 8)
+            if w == 1:
+                draw.ellipse([(x*square_size+side_space+piece_size_diff, y*square_size+side_space+piece_size_diff), ((x+1)*square_size+side_space-piece_size_diff, (y+1)*square_size+side_space-piece_size_diff)], Black)
+            elif w == 2:
+                draw.ellipse([(x*square_size+side_space+piece_size_diff, y*square_size+side_space+piece_size_diff), ((x+1)*square_size+side_space-piece_size_diff, (y+1)*square_size+side_space-piece_size_diff)], White)
+            self.board_images[timestump][v] = board_image
+
+    def update_board_images(self):
+        timestump = str(time.time()).replace(".","")
+        self.board_images[timestump] = {}
+        for i, v in enumerate(self.board):
+            self.put_piece_images(i, v, timestump)
+        return timestump
+
+    def able_to_put(self, w=None):
+        if w is None:
+            w = self.turn
+        squares = []
+        for i in range(64):
+            if self.board[i] != 0: continue
+            try:
+                if self.put_piece(i, w, False) != 0:
+                    squares.append(i)
+            except: pass
+        return squares
+
+    def ai_turn_proccess(self):
+        p = self.best(self.ai_turn)
+        self.put_piece(p, self.ai_turn)
+
+def create_board():
+    a = np.zeros(64, dtype=int)
+    a[27] = a[36] = 1
+    a[28] = a[35] = 2
+    return a
+def print_board(a):
+    print('  a b c d e f g h')
+    for i in range(8):
+        print(i+1, end=' ')
+        print(' '.join('.*o'[j] for j in a[i*8:][:8]))
+def put_piece(a, p, w, puton=True, chk=True):
+    t, x, y = 0, p%8, p//8
+    for di, fi in zip([-1, 0, 1], [x, 7, 7-x]):
+        for dj, fj in zip([-8, 0, 8], [y, 7, 7-y]):
+            if not di == dj == 0:
+                b = a[p+di+dj::di+dj][:min(fi, fj)]
+                n = (b==3-w).cumprod().sum()
+                if b.size <= n or b[n] != w: n = 0
+                t += n
+                if puton:
+                    b[:n] = w
+    if puton:
+        if chk: assert(a[p] == 0 and t > 0)
+        a[p] = w
+    return t
+
+def best(a, w):
+    from math import exp
+    r, b, c = [], a.copy(), 1+exp(-np.count_nonzero(a)/16)
+    for i in range(64):
+        if b[i] != 0: continue
+        t = put_piece(b, i, w, True, False)
+        if t == 0:
+            b[i] = 0
+            continue
+        u = sum(b[j]==0 and put_piece(b, j, 3-w, False) > 0 for j in range(64))
+        r.append((t-c*u+np.random.rand()*0.5, i))
+        b = a.copy()
+    return sorted(r)[-1][1] if r else -1
+
+if __name__ == '__main__':
+    a = create_board()
+    w = 1
+    while np.count_nonzero(a) < 64:
+        print_board(a)
+        s = input('> ')
+        if not s or s=='q': break
+        if s != 'p':
+            try:
+                x, y = ord(s[0])-97, int(s[1])-1
+                put_piece(a, x+8*y, w)
+            except:
+                continue
+        p = best(a, 3-w)
+        if p >= 0:
+            put_piece(a, p, 3-w)
+    print_board(a)
+    n1, n2 = (a==1).sum(), (a==2).sum()
+    print('%d - %d %s' % (n1, n2,
+        'You win' if n1 > n2 else
+        'You lose' if n1 < n2 else 'Draw'))
