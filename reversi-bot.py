22c6c28193ecad683398b47b01e8677e6ebf3116
naarisan <qazaqaza98@gmail.com>
2016-11-24 15:18:06 +0900

    pdebug

--- reversi-bot.py
+++ reversi-bot.py
@@ -12,10 +12,10 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
+from __future__ import unicode_literals
+
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-
-from __future__ import unicode_literals
 
 import errno
 import os
