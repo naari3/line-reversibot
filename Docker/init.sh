50cefcd41737a194f252a8fda7aa7c9cee157824
naarisan <qazaqaza98@gmail.com>
2016-11-26 19:32:18 +0900

    gunicorn running on forground

--- Docker/init.sh
+++ Docker/init.sh
@@ -2,6 +2,8 @@
 service postgresql start
 su postgres -c 'echo "docker" | psql -f /initdb.sql'
 service nginx start
-service supervisor start
+# service supervisor start
 # supervisorctl start reversi-bot # already started
 # python3 reversi-bot.py
+cd /app
+DATABASE_URL="postgres://docker:docker@127.0.0.1:5432/reversi_db" /usr/local/bin/gunicorn reversi-bot:app --config /app/gunicorn-config.py
